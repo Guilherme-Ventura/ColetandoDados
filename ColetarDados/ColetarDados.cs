59ecf03ffc8ae6ea05b3f7d5354e340270b583e3
Guilherme-Ventura <guilherme.almeida.ventura@gmail.com>
2022-01-20 12:19:32 -0300

    Primeiro commit

---
+++ ColetarDados/ColetarDados.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColetarDados
+{
+    public class ColetarDados
+    {
+        public DadosInvocador BuscarRequisicao(string nickname)
+        {
+            var client = new RestClient("https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"+nickname);
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("X-Riot-Token", "RGAPI-9884381c-ff93-4f6d-804a-39399c16feca");
+            IRestResponse response = client.Execute(request);
+            //Console.WriteLine(response.Content);
+
+            DadosInvocador saida = JsonConvert.DeserializeObject<DadosInvocador>(response.Content);
+
+            return saida;
+        }
+        public List<DadosPerfil> BuscarPerfil(string id)
+        {
+            var client = new RestClient("https://br1.api.riotgames.com/lol/league/v4/entries/by-summoner/"+id);
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("X-Riot-Token", "RGAPI-9884381c-ff93-4f6d-804a-39399c16feca");
+            IRestResponse response = client.Execute(request);
+
+            List<DadosPerfil> saida = JsonConvert.DeserializeObject<List<DadosPerfil>>(response.Content);
+
+            return saida;
+        }
+    }
+}
---
+++ ColetarDados/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColetarDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite nickname:");
+            string linha = Console.ReadLine();
+
+            ColetarDados executa = new ColetarDados();
+
+            DadosInvocador dados = executa.BuscarRequisicao(linha);
+
+            List<DadosPerfil> dados2 = executa.BuscarPerfil(dados.id);
+
+            // string nickname = "Takato";
+            Console.WriteLine("Nome: " + dados.name);
+            Console.WriteLine($"level: {dados.summonerLevel}");
+            Console.WriteLine($"Elo: {dados2[0].tier} {dados2[0].rank}");
+            Console.WriteLine($"PDL: {dados2[0].leaguePoints}");
+            Console.WriteLine($"Vitorias: {dados2[0].wins}");
+            Console.WriteLine($"Derrotas: {dados2[0].losses}");
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
